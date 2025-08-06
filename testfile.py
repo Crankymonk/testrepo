d5736a1ad1e27e227c9f89e085390090f4f1dab1
Crankymonk <62638129+Crankymonk@users.noreply.github.com>
2025-08-06 15:19:24 +0300

    Add testfile.py
    Counting clicker

--- testfile.py
+++ testfile.py
@@ -1,2 +1,15 @@
-print("Test")
-print("Test again")
+class CountingClicker:
+    def __init__(self, count: int = 0):
+        self.count = count
+
+    def __repr__(self):
+        return f"CountingClicker(count={self.count})"
+
+    def click(self, num_times = 1):
+        self.count += num_times
+
+    def read(self):
+        return self.count
+
+    def reset(self):
+        self.count = 0
